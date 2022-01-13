665cc408a843c9067122be49d5acd665b5aad285
AnasJMirza <ajmhimself@gmail.com>
2022-01-13 23:45:13 +0500

    Ques No 15 Done

---
+++ Assignment-04 (Arrays)/src/com/anas/Q14.java
@@ -0,0 +1,32 @@
+package com.anas;
+
+import java.util.Scanner;
+
+public class Q14 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows : ");
+        int m = in.nextInt();
+
+        System.out.print("Enter the number of Columns : ");
+        int n = in.nextInt();
+
+        int[][] indices = {{0, 1},{1, 1}};
+
+//        oddCells(m, n, indices);
+
+    }
+
+
+//    static int oddCells(int m, int n, int[][] indices) {
+//        int[][] arr = new int[m][n];
+//
+//        for (int i = 0; i < indices.length; i++) {
+//            for (int j = 0; j < indices[i].length; j++) {
+////                arr[]
+//            }
+//        }
+//
+//    }
+}
