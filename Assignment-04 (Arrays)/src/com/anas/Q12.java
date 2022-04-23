8bea845db5e4210b8a3b5886bf06017856a15c85
AnasJMirza <ajmhimself@gmail.com>
2022-04-23 12:21:40 +0500

    Update README

--- Assignment-04 (Arrays)/src/com/anas/Q12.java
+++ Assignment-04 (Arrays)/src/com/anas/Q12.java
@@ -1,2 +1,22 @@
-package com.anas;public class Q12 {
+package com.anas;
+//1732. Find the Highest Altitude
+
+public class Q12 {
+    public static void main(String[] args) {
+        int[] gain = {-4,-3,-2,-1,4,3,2};
+        System.out.println(largestAltitude(gain));
+    }
+
+    static int largestAltitude(int[] gain){
+        int maxAltitude = 0;
+        int check = 0;
+        for (int i = 0; i < gain.length; i++) {
+            check = check + gain[i];
+            if (maxAltitude < check) {
+                maxAltitude = check;
+            }
+        }
+
+        return maxAltitude;
+    }
 }
