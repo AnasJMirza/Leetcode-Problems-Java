fdd127c78878d83be74c2e8c49407a12f19f7273
AnasJMirza <ajmhimself@gmail.com>
2022-09-25 12:21:55 +0500

    Q6

--- Intro to Arrays/src/com/anas/Q6.java
+++ Intro to Arrays/src/com/anas/Q6.java
@@ -1,7 +1,35 @@
 package com.anas;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //
 public class Q6 {
     public static void main(String[] args) {
-        int[] candies = {2,3,5,1,3};
+        int[] candies = {4,2,1,1,2};
+        int extraCandies = 1;
+        System.out.println(kidsWithCandies(candies, extraCandies));
+
+    }
+
+    static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        ArrayList<Boolean> output = new ArrayList<Boolean>(candies.length);
+
+        for (int i = 0; i < candies.length; i++) {
+            int max = candies[i] + extraCandies;
+            boolean flag = false;
+            for (int j = 0; j < candies.length; j++) {
+                if (max < candies[j]){
+                    output.add(i, false);
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == false){
+                output.add(i, true);
+            }
+        }
+        return output;
     }
 }
