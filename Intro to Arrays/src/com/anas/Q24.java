9ebada965a9835b940c3d0d3659b57e130132d47
AnasJMirza <ajmhimself@gmail.com>
2022-10-24 16:21:04 +0500

    Q24

--- Intro to Arrays/src/com/anas/Q24.java
+++ Intro to Arrays/src/com/anas/Q24.java
@@ -6,25 +6,24 @@ public class Q24 {
     public static void main(String[] args) {
         int[] nums = {5,4,-1,7,8};
         maxSubArray(nums);
+
     }
 
-    static int maxSubArray(int[] nums){
-        int maxSum = Integer.MIN_VALUE;
+    static int maxSubArray(int[] nums) {
+        int maxSub = nums[0];
+        int curSum = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-
-            for (int j = i; j < nums.length; j++) {
-
-                sum = sum + nums[j];
-                if (sum > maxSum){
-                    maxSum = sum;
-                }
+        for (int i = 0; i < nums.length; i++){
+            if (curSum < 0){
+                curSum = 0;
             }
+
+            curSum += nums[i];
+
+            maxSub = Math.max(maxSub, curSum);
+
+
         }
-
-        System.out.println(maxSum);
-
-        return 1;
+        return maxSub;
     }
 }
