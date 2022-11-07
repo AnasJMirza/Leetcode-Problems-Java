bc6f50b82d98fa6ea89780fdcc59fe119a3c2a57
AnasJMirza <ajmhimself@gmail.com>
2022-11-08 00:01:46 +0500

    BinarySearchDescending

--- Searching/src/com/anas/BinarySearch.java
+++ Searching/src/com/anas/BinarySearch.java
@@ -3,22 +3,42 @@ package com.anas;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {2,4,8,9,12,15,45,57,89,100};
-        int target = 15;
+        int[] arr2 = {100, 90,80,45,34,32,22,11,5,2,1};
+        int target = 1;
         System.out.println(binarySearch(arr, target));
+        System.out.println(binarySearchReverse(arr2, target));
     }
 
     static int binarySearch(int[] arr, int target){
         int start = 0;
         int end = arr.length - 1;
 
-        for (int index = 0; index < arr.length / 2; index++) {
+        while (start <= end){
             int mid = (start + end) / 2;
             if (arr[mid] == target){
                 return mid;
             }else if  (arr[mid] < target){
-                start += mid;
+                start = mid + 1;
             }else{
-                end -= mid;
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    static int binarySearchReverse(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end){
+            int mid = (start + end) / 2;
+            if (arr[mid] == target){
+                return mid;
+            }else if  (arr[mid] < target){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
             }
         }
 
