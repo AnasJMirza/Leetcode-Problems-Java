dc60cda589c70d0139812f08fa29ab898a940720
AnasJMirza <ajmhimself@gmail.com>
2022-11-12 03:44:52 +0500

    valid perfect square

--- Searching/src/com/anas/validPerfectSquare.java
+++ Searching/src/com/anas/validPerfectSquare.java
@@ -2,20 +2,24 @@ package com.anas;
 
 public class validPerfectSquare {
     public static void main(String[] args) {
-        System.out.println(isPerfectSquare(1));
+        System.out.println(isPerfectSquare(2147483647));
     }
 
+
     static boolean isPerfectSquare(int num) {
-        int ans = num;
+        long start = 0;
+        long end = num;
 
-        while (ans > 0) {
-
-            if (ans * ans == num){
+        while (start <= end) {
+            long mid = start + (end - start) / 2;
+            long sqrt = mid * mid;
+            if (sqrt == num) {
                 return true;
+            } else if (sqrt > num) {
+                end = mid - 1;
             } else {
-                ans = ans / 2;
+                start = mid + 1;
             }
-
         }
 
         return false;
