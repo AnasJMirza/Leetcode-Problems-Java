16937bff814a957495fc2e96d88f21c06914200c
AnasJMirza <ajmhimself@gmail.com>
2022-11-13 00:41:48 +0500

    row-col sorted matrix

--- Searching/src/com/anas/BinarySearch2D.java
+++ Searching/src/com/anas/BinarySearch2D.java
@@ -4,14 +4,15 @@ import java.util.Arrays;
 
 public class BinarySearch2D {
     public static void main(String[] args) {
-        int[][] arr = {
-                {10, 20, 30, 40},
-                {15, 25, 35, 45},
-                {28, 29, 37, 49},
-                {33, 34, 38, 50}
-        };
+//        int[][] arr = {
+//                {10, 20, 30, 40},
+//                {15, 25, 35, 45},
+//                {28, 29, 37, 49},
+//                {33, 34, 38, 50}
+//        };
 
-        System.out.println(Arrays.toString(BS2D(arr, 34)));
+
+        System.out.println(Arrays.toString(BS2D(arr, 15)));
 
     }
 
