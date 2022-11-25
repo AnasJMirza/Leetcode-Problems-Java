d1a7f0a5384e99a47c81130f22f54895e11d7799
AnasJMirza <ajmhimself@gmail.com>
2022-11-25 12:47:31 +0500

    insertion sort + bubble sort

--- Sorting/src/com/anas/InsertionSort.java
+++ Sorting/src/com/anas/InsertionSort.java
@@ -2,15 +2,15 @@ package com.anas;
 
 import java.util.Arrays;
 
-public class SelectionSort {
+public class InsertionSort {
 
     public static void main(String[] args) {
 	    int[] arr = {5, -1, 0, 0, -2, -2, 3, 3, 45};
-        selectionSort(arr);
+        insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static int[] selectionSort(int[] arr) {
+    static int[] insertionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
