6e13312d51f42489e3e42a920c8be5e18649fd25
AnasJMirza <ajmhimself@gmail.com>
2022-11-26 10:37:31 +0500

    missing number - easy

---
+++ Sorting/src/com/anas/CycleSort.java
@@ -0,0 +1,34 @@
+package com.anas;
+
+import java.util.Arrays;
+
+
+
+public class CycleSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1 };
+        cycleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cycleSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+
+
+}
