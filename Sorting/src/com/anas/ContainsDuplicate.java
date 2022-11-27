b4b83a19dbe35091d3d6d149f7dd901d723497a3
AnasJMirza <ajmhimself@gmail.com>
2022-11-27 19:09:41 +0500

    contains duplicate

---
+++ Sorting/src/com/anas/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+package com.anas;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+
+    }
+
+    static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
